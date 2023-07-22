4deab3b1c1765a41b565e4007b11c9232e247419
Mikk0git <kasprzakmilan1@gmail.com>
2023-07-22 15:12:37 +0200

    Snake head is moving

--- src/main.js
+++ src/main.js
@@ -1,38 +1,55 @@
 
-function setCoordinates(x,y,id) {
-    document.getElementById(id).style["grid-column-start"] = x;
-    document.getElementById(id).style["grid-row-start"] = y;
-    console.log("Coordinates set to X:"+x+" Y:"+y)
+function setCoordinates(coordinates,id) {
+    document.getElementById(id).style["grid-column-start"] = coordinates[0];
+    document.getElementById(id).style["grid-row-start"] = coordinates[1];
+    console.log("Coordinates set to X:"+coordinates[0]+" Y:"+coordinates[1])
     
 }
 
 
-function moveCell(x,y,id,direction){
-    console.debug(`X:${x} Y:${y} id:${id} dir:${direction}`)
-    switch(direction){
+function moveCell(coordinates, id, direction) {
+    console.debug(`X:${coordinates[0]} Y:${coordinates[1]} id:${id} dir:${direction}`);
+    switch (direction) {
         case 1:
-            setCoordinates(x,y-1,id)
-            console.log(id+"move up")
+            setCoordinates([coordinates[0], coordinates[1] - 1], id);
+            console.log(id + " move up");
             break;
         case 2:
-            setCoordinates(x+1,y,id)
-            console.log(id+"move right")
+            setCoordinates([coordinates[0] + 1, coordinates[1]], id);
+            console.log(id + " move right");
             break;
         case 3:
-            setCoordinates(x,y+1,id)
-            console.log(id+"move down")
+            setCoordinates([coordinates[0], coordinates[1] + 1], id);
+            console.log(id + " move down");
             break;
         case 4:
-            setCoordinates(x-1,y,id)
-            console.log(id+"move left")
+            setCoordinates([coordinates[0] - 1, coordinates[1]], id);
+            console.log(id + " move left");
             break;
-        
-
     }
-
 }
 
 
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
 
-moveCell(5,1,'snakeHead',3)
+function getCoordinates(id){
+    x=Number(document.getElementById(id).style["grid-column-start"])
+    y=Number(document.getElementById(id).style["grid-row-start"])
+    coordinates = [x,y]
+    console.log(`Coordinates = ${coordinates}`)
+    return coordinates
+}
+
+async function  startGame(){
+    let alive = 1
+    setCoordinates([1,4], "snakeHead")
+    while (alive == 1){
+        await sleep(1000)
+        moveCell(getCoordinates('snakeHead'),'snakeHead',3)
+
+    }
+}
+startGame()
 
