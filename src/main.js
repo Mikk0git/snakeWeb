f4f39f950ef3c6555443afce2b0166a08ffbf34b
Mikk0git <kasprzakmilan1@gmail.com>
2023-07-22 14:15:49 +0200

    moveCell

--- src/main.js
+++ src/main.js
@@ -2,7 +2,37 @@
 function setCoordinates(x,y,id) {
     document.getElementById(id).style["grid-column-start"] = x;
     document.getElementById(id).style["grid-row-start"] = y;
+    console.log("Coordinates set to X:"+x+" Y:"+y)
     
 }
 
-setCoordinates(7,9,"snakeHead")
+
+function moveCell(x,y,id,direction){
+    console.debug(`X:${x} Y:${y} id:${id} dir:${direction}`)
+    switch(direction){
+        case 1:
+            setCoordinates(x,y-1,id)
+            console.log(id+"move up")
+            break;
+        case 2:
+            setCoordinates(x+1,y,id)
+            console.log(id+"move right")
+            break;
+        case 3:
+            setCoordinates(x,y+1,id)
+            console.log(id+"move down")
+            break;
+        case 4:
+            setCoordinates(x-1,y,id)
+            console.log(id+"move left")
+            break;
+        
+
+    }
+
+}
+
+
+
+moveCell(5,1,'snakeHead',3)
+
